d0149efc8be0f31ac067567393ea41739fd7431b
Christian Brill <christiangbrill@gmail.com>
2017-04-13 09:57:05 +0200

    created route for signup

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,9 @@
 <?php
-	
+
 	$w_routes = array(
+		// osco.dev/home/
 		['GET', '/', 'Default#home', 'default_home'],
+
+		// osco.dev/user/signup/
+		['GET', 'user/signup/', 'User#signup', 'user_signup'],
 	);
