797326a267c8151ef8924ef7c97d5f32d6a60755
Naomi Corman <naomicorman@gmail.com>
2017-04-13 15:27:10 +0200

    Layout - Link to Sign Up in navigation bar

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -13,59 +13,57 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/naomi.min.css') ?>">
 </head>
 <body>
-	<div class="container">
-		<header>
-			<nav>
-				<h1>OSCo</h1>
-				<ul>
-					<a href="">
-						<li>Stories</li>
-					</a>
-					<a href="">
-						<li>Blog</li>
-					</a>
-					<a href="">
-						<li>Support</li>
-					</a>
-					<a href="">
-						<li>About OSCo</li>
-					</a>
-					<a href="">
-						<li>Log In</li>
-					</a>
-					<a href="<?= $this->url("user_signup") ?>">
-						<li>Sign Up</li>
-					</a>
-				</ul>
-				<!--<i class='fa fa-search fa-lg' aria-hidden='true'></i>-->
-				<form action="" method="get">
-				<input type="text" name="searchInput" placeholder="Search">
-				</form>
-			</nav>
-		</header>
+	<header>
+		<nav>
+			<h1>OSCo</h1>
+			<ul>
+				<a href="">
+					<li>Stories</li>
+				</a>
+				<a href="">
+					<li>Blog</li>
+				</a>
+				<a href="">
+					<li>Support</li>
+				</a>
+				<a href="">
+					<li>About OSCo</li>
+				</a>
+				<a href="">
+					<li>Log In</li>
+				</a>
+				<a href="<?= $this->url("user_signup") ?>">
+					<li>Sign Up</li>
+				</a>
+			</ul>
+			<!--<i class='fa fa-search fa-lg' aria-hidden='true'></i>-->
+			<form action="" method="get">
+			<input type="text" name="searchInput" placeholder="Search">
+			</form>
+		</nav>
+	</header>
 
-		<section id="main">
-			<?= $this->section('main_content') ?>
-		</section>
+	<section id="main">
+		<?= $this->section('main_content') ?>
+	</section>
 
-		<footer>
-			<div id="footerContent">
-				<div>
-					<h4><a href="">About</a></h4>
-					<p>OSCo is blubb.</p>
-					<h4>Disclaimer</h4>
-					<p>Blubb</p>
-					<h4>&copy; Copyright</h4>
-				</div>
-				<div>
-					<h4><a href="">Contact</a></h4>
-					<p>osco@contact.lu</p>
-					<p>Facebook</p>
-					<p>Twitter</p>
-				</div>
+	<footer>
+		<div id="footerContent">
+			<div>
+				<h4><a href="">About</a></h4>
+				<p>OSCo is blubb.</p>
+				<h4>Disclaimer</h4>
+				<p>Blubb</p>
+				<h4>&copy; Copyright</h4>
 			</div>
-		</footer>
-	</div>
+			<div>
+				<h4><a href="">Contact</a></h4>
+				<p>osco@contact.lu</p>
+				<p>Facebook</p>
+				<p>Twitter</p>
+			</div>
+		</div>
+	</footer>
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	
