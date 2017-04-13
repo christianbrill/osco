0e65800b07afd25b6feedf985de18fc829b5c3bb
Naomi Corman <naomicorman@gmail.com>
2017-04-13 15:08:00 +0200

    Layout - trying to fix bugs by adding Bootstrap CDN and removing normalize and reset

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -3,10 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/reset.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/normalize.css') ?>css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/bootstrap.css') ?>css/bootstrap.css">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/naomi.min.css') ?>">
 </head>
@@ -31,7 +33,7 @@
 					<a href="">
 						<li>Log In</li>
 					</a>
-					<a href="">
+					<a href="<?= $this->url("user_signup") ?>">
 						<li>Sign Up</li>
 					</a>
 				</ul>
