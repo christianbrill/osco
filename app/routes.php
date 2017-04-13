9af9aa73174231966e5440c82c3cd9669838fbbc
Christian Brill <christiangbrill@gmail.com>
2017-04-13 11:39:39 +0200

    added signup validation functionalities and created login and signup views

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,9 @@
 		// osco.dev/home/
 		['GET', '/', 'Default#home', 'default_home'],
 
-		// osco.dev/user/signup/
+		// osco.dev/signup/
 		['GET|POST', '/signup/', 'User#signup', 'user_signup'],
+
+		// osco.dev/login/
+		['GET', '/login/', 'User#login', 'user_login'],
 	);
