00e9b690d3f1cb286d2ea697b486891e5b4c16f8
Max Mehlinger <maximmehlinger@hotmail.com>
2017-04-17 18:13:09 +0200

    about page showing/finally

--- app/Views/about/about.php
+++ app/Views/about/about.php
@@ -1,12 +1,14 @@
 
 
 <?php $this->layout('layout', ['title' => 'About', 'currentPage' => 'about']) ?>
-<a href="<?= $this->url('about') ?>">About</a>
-<?php>require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;?>
-<?php $this->start('main_content') ?>
 
-<?php if ($displayContactForm) : ?>
+<?php $this->start('main_content') ?>
+<div class="well"><h1>About Osco</h1><br>
+Osco is.......  <br>
+<br>
+<br>
+<br></div>
+
     <form action="email.php" method="post">
         <div class="form-group">
             <input type="email" class="form-control" name="contactEmail" value="" placeholder="Your email address" />
@@ -21,14 +23,14 @@ $mail = new PHPMailer;?>
         </div>
 
         <div class="form-group">
-            <textarea rows="20" cols="150" class="form-control" name="contactMessage" value="" placeholder="Your message" cols='80' rows='50' >Enter text here...</textarea>
+            <textarea class="form-control" name="contactMessage" value="" placeholder="Your message" cols='80' rows='3' >Enter text here...</textarea>
         </div>
 
         <div class="form-group">
             <input type="submit" class="btn btn-success btn-block" value="Send message" />
         </div>
     </form>
-<?php endif; ?>
+
 
 <?php $this->stop('main_content') ?>
 
