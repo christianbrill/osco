963cb6d1cc247ea69afccf37ad24bd88ab026bc4
Pedro H G B Queiroz <barrosoph@gmail.com>
2017-04-17 18:15:09 +0200

    Ajax, Home, CSS

--- app/Views/content/home.php
+++ app/Views/content/home.php
@@ -1,14 +1,13 @@
 <?php $this->layout('layout', ['title' => 'Home', 'currentPage' => 'home']) ?>
 
 <?php $this->start('main_content') ?>
-	<?php debug($w_user); ?>
 
 	<section id="ajaxHomeStories">
 	<?php foreach($randomStories as $story) : ?>
-		<article >
+		<article id="storyBox">
 			<a href="#">
 				<div>
-					<h1><?= \Controller\ContentController::getShortTitle($story['sto_title']) ?></h1>
+					<h1 id="title"><?= \Controller\ContentController::getShortTitle($story['sto_title']) ?></h1>
 					<p><?= \Controller\ContentController::getShortDescription($story['sto_content'])?></p>
 				</div>
 			</a>
@@ -16,5 +15,17 @@
 	<?php endforeach; ?>
 	</section>
 	<button id="refreshStories">Refresh Stories</button>
+
+	<!--Here starts the "Need Help" box -->
+
+	<section id="rightColumn">
+		<a href="#">		
+			<div id="needHelpBox">
+				<h1>Need Help?</h1>
+				<p>OSCo has the contact information you need to get help. If you prefer, we also provide a form for you to get anonymous help-we handle the contact for you.
+				<br>You don't need to register to get help. Click this box, or the icon "Need Help" in the nav bar from any page, and get the support you need.</p>
+			</div>
+		</a>
+	</section>
 
 <?php $this->stop('main_content') ?>
