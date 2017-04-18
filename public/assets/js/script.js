35913e8220d6ff1b90b091f65ee0c5dad17de69e
Pedro H G B Queiroz <barrosoph@gmail.com>
2017-04-18 14:21:14 +0200

    Update to retrieve page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,22 +11,25 @@ $(document).ready(function(){
 
 function refreshStories() {
 
-	var $story = $('#storyBox');
 	//var $storyArray = $.map($story, function(el) { return el });
 
 	$.ajax({  	
-		url: '/osco/public/ajax/home/',		
+		url: '/osco/public/ajax/home/',	
+
 }).done(function(response) {
-	console.log(response);
-	
+	//console.log(response);
+
+	//var array = Array.from(response); 
+	//console.log(array);
 	//JSON.stringify(response);
     //$story.empty().load("/osco/public/ajax/home/ #ajaxHomeStories");
 
-		var parsed = JSON.parse(response);
+   	$("#ajaxHomeStories").empty().load("/osco/public/ajax/home/ #storyBox");
 
 
-
-   		$story.empty().load('/osco/public/ajax/home/');
+   	//var info = '<?php foreach($randomStories as $story) : ?> <article id="storyBox"><a href="#"><div><h1 id="title"><?= \Controller\ContentController::getShortTitle($story["sto_title"]) ?></h1><p><?= \Controller\ContentController::getShortDescription($story["sto_content"])?></p></div></a></article><?php endforeach; ?>';
+   	
+   	//$("#ajaxHomeStories").append(info);
 
 	});
 }//refreshStories function end
