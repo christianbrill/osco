0ab6518e36791622818aca6e7edc18b98f9914a4
Naomi Corman <naomicorman@gmail.com>
2017-04-20 09:47:23 +0200

    Home link to story details

--- app/Views/content/home.php
+++ app/Views/content/home.php
@@ -19,7 +19,7 @@
 	<section id="ajaxHomeStories">
 	<?php foreach($randomStories as $story) : ?>
 		<article id="storyBox">
-			<a href="">
+			<a href="<?= $this->url('content_story',['id' => $story['sto_id']]); ?>">
 				<div>
 					<h1 id="title"><?= \Controller\ContentController::getShortTitle($story['sto_title']) ?></h1>
 					<p><?= \Controller\ContentController::getShortDescription($story['sto_content'])?></p>
